5711331baa77035d1dbeea517e322746cc5d16df
hantao2015 <hantao@realsun.biz>
2018-06-27 14:39:49 +0800

    fix

--- RealsunSynDataServerGuard/Form1.vb
+++ RealsunSynDataServerGuard/Form1.vb
@@ -2,6 +2,7 @@
 Imports System.IO
 
 Public Class RealsunSynDataServerGuard
+    Private _predate As Date
 
     Private Sub RealsunMiniServerGuard_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
         e.Cancel = True
@@ -14,31 +15,39 @@ Public Class RealsunSynDataServerGuard
         Me.Hide()
         Me.ShowInTaskbar = False
         Me.NotifyIcon1.Visible = True
+        _predate = DateTime.Now.Date
         Timer1.Start()
     End Sub
     Private Sub killprocess()
         Dim process As Process() = System.Diagnostics.Process.GetProcesses()
+        Try
+            For Each prc As Process In process
+                If prc.ProcessName = "RealsunSynData" Then
+                    prc.Kill()
+
+                End If
+            Next
+        Catch ex As Exception
+
+        End Try
 
 
-        For Each prc As Process In process
-            If prc.ProcessName = "RealsunSynData" Then
-                '  prc.Kill()
-
-            End If
-        Next
 
     End Sub
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-
+        Dim H As Integer = DateAndTime.Now.Hour
         Dim create As Boolean = False
         Try
             If System.Diagnostics.Process.GetProcessesByName("RealsunSynData").Length <= 0 Then
                 System.Diagnostics.Process.Start("RealsunSynData.exe")
+            Else
+                If DateDiff(DateInterval.Day, _predate, DateAndTime.Now.Date) <> 0 Then
+                    killprocess()
+                    System.Diagnostics.Process.Start("RealsunSynData.exe")
+                    _predate = DateAndTime.Now.Date
+                End If
             End If
-            Dim H As Integer = DateAndTime.Now.Hour
-            If H = 2 Then
-                '   killprocess()
-            End If
+
 
         Catch ex As Exception
 
